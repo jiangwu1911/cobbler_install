c2957036541d697b2c0f77e7c1096edbbd055bea
wujiang <jiangwu1911@hotmail.com>
2015-05-08 08:19:59 +0800

    fix

--- esxi5.5/import_esxi.sh
+++ esxi5.5/import_esxi.sh
@@ -3,7 +3,7 @@
 mkdir esxi55
 mount -o loop VMware-VMvisor-Installer-201410001-2143827.x86_64.iso esxi55
 current_dir=`pwd`
-#cobbler import --path=${current_dir}/esxi55 --name=esxi55
+cobbler import --path=${current_dir}/esxi55 --name=esxi55
 
 cp esxi55.ks /var/lib/cobbler/kickstarts/
 umount esxi55
